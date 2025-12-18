c8b78ce8779924d6e32a81b0d30d6c1b7316e2d9
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2025-12-18 00:35:00 -0500

    quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import InteractiveMap from './components/Map'
 import Article from './components/Article';
 
 export default function App() {
-    const [renderArticle, setRenderArticle] = useState(true);
+    const [renderArticle, setRenderArticle] = useState(false);
 
     document.body.style.overflow = renderArticle ? 'auto' : 'hidden';
     return (
